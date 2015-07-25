89b76f45b17d46d2b200317c6fe0f548db4cf698
Marco Paladini <marco.paladini@ocado.com>
2015-07-25 13:04:51 +0100

    urls and views for a hello world

--- splitbill/views.py
+++ splitbill/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
+from .forms import UploadFileForm
 
 # Create your views here.
+def home(request):
+  context = {'name':'world'}
+  return render_to_response('splitbill/index.html', context)
+  
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render_to_response('upload.html', {'form': form})
