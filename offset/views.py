2449d1a772b331c8306594c64d275de07c251c84
Marco Paladini <paladini.marco@gmail.com>
2016-10-16 12:22:22 +0100

    use list of orders to build list of transactions

--- offset/views.py
+++ offset/views.py
@@ -2,13 +2,40 @@
 # -*- coding: utf-8 -*-
 
 # Create your views here.
+from offset.models import Order
 from offset.models import Transaction
 from offset.models import Mortgage
-from django.shortcuts import render_to_response
-from datetime import date
+from django.shortcuts import render
+from datetime import date, timedelta
+from dateutil import relativedelta
 from utils import MortgageCalculator
 import locale
 locale.setlocale(locale.LC_ALL, '')
+
+
+def build_transaction_list(mortgage):
+    transactions = []    
+    order_list_r = Order.objects.all().order_by('-date')    
+    # bank orders are until the mortgage term, or until the next order
+    end_date = mortgage.start_date + relativedelta.relativedelta(months=mortgage.term)
+    for o in order_list_r:
+        period = []
+        if o.type=="MONTHLY":
+            delta = relativedelta.relativedelta(end_date, o.date)
+            months = delta.years*12 + delta.months
+            for m in range(months):
+                t = Transaction()
+                t.date = o.date + relativedelta.relativedelta(months=m)
+                t.type = "MONTHLY"
+                t.amount = o.amount
+                t.mtg = mortgage
+                period.append(t)
+        end_date = o.date
+        period.reverse()
+        transactions += period
+    
+    transactions.reverse()
+    return transactions
 
 def calculate():
     output = ""
@@ -16,16 +43,16 @@ def calculate():
     <tr><td>Date</td><td>Type</td><td>Amount</td>
     <td>Balance</td><td>Pot</td><td>Interest paid</td>
     <td>Interest to pot</td><td>Paid capital</td><td>Paid pot</td></tr>
-    """
-    transactions_list = Transaction.objects.all().order_by('date')
+    """    
     mtg = Mortgage.objects.all()[0]
+    transactions_list = build_transaction_list(mtg) #Transaction.objects.all().order_by('date')
     mtgcalc = MortgageCalculator(balance=mtg.initial_balance, rate=mtg.initial_rate, term=mtg.term, d0=mtg.start_date)
     for t in transactions_list:
         mtgcalc.do_transaction(t)
         display += "<tr><td>"+str(t.date)+"</td><td>"+t.type+"</td>"
         display += "<td>"+str(t.amount)+"</td>"
-        display += "<td>"+locale.currency(round(mtgcalc.c)/100.0,grouping=True)[1:]+"</td>"
-        display += "<td>"+locale.currency(round(mtgcalc.s)/100.0,grouping=True)[1:]+"</td>"
+        display += "<td>"+str(round(mtgcalc.c)/100.0)+"</td>"
+        display += "<td>"+str(round(mtgcalc.s)/100.0)+"</td>"
         display += "<td>"+str(mtgcalc.interest_paid)+"</td>"
         display += "<td>"+str(mtgcalc.interest_saved)+"</td>"
         display += "<td>"+str(mtgcalc.to_capital)+"</td>"
@@ -38,19 +65,16 @@ def calculate():
 
 def home(request):
     mtg_list = Mortgage.objects.all()
-    return render_to_response('offset/index.html', {'mtg_list': mtg_list})
+    return render(request, 'offset/index.html', context={'mtg_list': mtg_list})
 
 
 def mtg(request, id):
     transactions_list = Transaction.objects.filter(mtg_id=id).order_by('-date')
-    output = "<h1>Marco's mortgage calculator program</h1><hr>\n"
-    output += calculate()
-    context = {}
-    context['calculator'] = output
-    output = ""
-    output += '<h2>Transactions list</h2><br><ul>'
-    for t in transactions_list:
-        output += '<li>'+str(t.date)+' , '+str(t.type)+' , &pound;'+str(t.amount)+'</li>'
-    output += '</ul>'
-    context['transactions'] = output
-    return render_to_response('offset/mortgage.html', context)
+    output = calculate()    
+    # output = ""
+    # output += '<h2>Transactions list</h2><br><ul>'
+    # for t in transactions_list:
+    #     output += '<li>'+str(t.date)+' , '+str(t.type)+' , &pound;'+str(t.amount)+'</li>'
+    # output += '</ul>'
+    # context['transactions'] = output    
+    return render(request, 'offset/mortgage.html', {'table': output})
