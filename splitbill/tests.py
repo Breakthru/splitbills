bc098bf819b2dd8333a54e620ad46d628ddde964
Marco Paladini <paladini.marco@gmail.com>
2016-12-18 00:16:08 +0000

    moved ccparser test

--- splitbill/tests.py
+++ splitbill/tests.py
@@ -1,17 +1,7 @@
-# -*- coding: UTF-8 -*-
 from django.test import TestCase
-from StringIO import StringIO
-from ccparser import ccparser
 
 # Create your tests here.
-class CCParserTest(TestCase):
-    def test_example_report(self):
-        f = StringIO("""
-Transaction Date, Posting Date, Billing Amount, Merchant, Merchant City , Merchant State , Merchant Zip , Reference Number , Debit/Credit Flag , SICMCC Code
-19/09/2016,20/09/2016,£10.55,"MERCHANT A","Example PC",,PC0 5EA,1234567890,D,1234
-19/09/2016,20/09/2016,£5.69,"MERCHANT PLC SACA","Example",GBR,PC 8NB,123456789,D,1234
-""")
-        p = ccparser()
-        p.parseTescoBank(f)
-        self.assertEqual(len(p.transactions), 2)
-	self.assertTrue( "10.55" in p.transactions[0])
+class MyTest(TestCase):
+    def test_case(self):
+        self.assertEqual(1, 1)
+        
