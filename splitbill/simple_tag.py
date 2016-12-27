93451e35ff5a2b7868a1ea09d98779f197978558
Marco Paladini <marco.paladini@ocado.com>
2016-12-27 02:06:12 +0000

    new auto tag

--- splitbill/simple_tag.py
+++ splitbill/simple_tag.py
@@ -11,15 +11,20 @@ class SimpleTag:
             else:
                 tag, _ = Tag.objects.get_or_create(name="both")
                 transaction.tags.add(tag)
-                tag, _ = Tag.objects.get_or_create(name="shopping")
+                tag, _ = Tag.objects.get_or_create(name="food")
             transaction.tags.add(tag)
-        if "sainsburys" in simple_check or "tesco" in simple_check or "poundland" in simple_check or "iceland" in simple_check:
+        if "waitrose" in simple_check or "sainsburys" in simple_check or "tesco" in simple_check or "poundland" in simple_check or "iceland" in simple_check:
             tag, _ = Tag.objects.get_or_create(name="both")
             transaction.tags.add(tag)
-            tag, _ = Tag.objects.get_or_create(name="shopping")
+            tag, _ = Tag.objects.get_or_create(name="food")
             transaction.tags.add(tag)
         if "lul" in simple_check or "tsgn" in simple_check:
             tag, _ = Tag.objects.get_or_create(name="marco")
             transaction.tags.add(tag)
             tag, _ = Tag.objects.get_or_create(name="travel")
+            transaction.tags.add(tag)
+        if "subway" in simple_check or "bakehouse" in simple_check or "harpsfield" in simple_check or "red lion" in simple_check:
+            tag, _ = Tag.objects.get_or_create(name="marco")
+            transaction.tags.add(tag)
+            tag, _ = Tag.objects.get_or_create(name="pub")
             transaction.tags.add(tag)
