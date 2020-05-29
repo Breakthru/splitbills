60875cee92b9ced2120316de318f48e592c93937
Marco Paladini <marco@greyparrot.ai>
2020-05-29 18:05:11 +0100

    moved py scripts

---
+++ finance/README.md
@@ -0,0 +1,17 @@
+# IDEAS TODO:
+
+- Encrypt transaction lists
+  * save CSV files as encrypted
+  * save a password-protected key
+  * decrypt CSV files to temp location
+  * allow edit/inspection using a spreadsheet
+  * move the modifications from temp file to storage
+  * encrypt the new modified files
+
+- Use a GUI
+  * pyqt window
+  * download statements and drag them to the window
+  * convert and open spreadsheet
+  * encrypt and close on window close
+
+
---
+++ finance/test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import os
+from santander2csv import convert
+
+def test_conversion():
+    with open("test.txt", "w") as fp:
+        fp.write("""From: 01/01/2019 to 02/02/2020
+
+     Account: XXXX XXXX XXXX 4419
+
+    Date: 02/01/2020
+    Description: test description test description
+    Amount: -100.00
+    Balance: 200.00
+
+    Date: 01/01/2020
+    Description: test description test description
+    Amount: -100.00
+    Balance: 100.00
+                                                        """)
+
+    with open("test.txt",'r') as fp:
+        transactions = convert(fp)
+
+    # print transactions in reverse order
+    for i in range(len(transactions)-1,-1,-1):
+        print(transactions[i])
+
+    os.remove("test.txt")
+
+if __name__=="__main__":
+    test_conversion()
